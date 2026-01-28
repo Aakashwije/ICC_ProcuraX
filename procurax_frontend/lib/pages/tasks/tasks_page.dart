f0d223757af15c68aead8ad3e461bf4506e562ee
Aakash Wijesekara <aakashwije92@gmail.com>
2026-01-28 21:08:57 +0530

    front end folder strcuture

--- procurax_frontend/lib/pages/tasks/tasks_page.dart
+++ procurax_frontend/lib/pages/tasks/tasks_page.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:procurax_frontend/routes/app_routes.dart';
 import 'package:procurax_frontend/widgets/app_drawer.dart';
 
-class DashboardPage extends StatelessWidget {
-  const DashboardPage({super.key});
+class TasksPage extends StatelessWidget {
+  const TasksPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     const Color primaryBlue = Color(0xFF1F4CCF);
 
     return Scaffold(
-      drawer: AppDrawer(currentRoute: AppRoutes.dashboard),
+      drawer: AppDrawer(currentRoute: AppRoutes.tasks),
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
@@ -32,7 +32,7 @@ class DashboardPage extends StatelessWidget {
                   ),
                   const Spacer(),
                   const Text(
-                    "Dashboard",
+                    "Tasks",
                     style: TextStyle(
                       fontSize: 28,
                       fontWeight: FontWeight.w800,
@@ -48,10 +48,7 @@ class DashboardPage extends StatelessWidget {
 
               const Expanded(
                 child: Center(
-                  child: Text(
-                    "Dashboard",
-                    style: TextStyle(fontSize: 20),
-                  ),
+                  child: Text("Tasks", style: TextStyle(fontSize: 20)),
                 ),
               ),
             ],
