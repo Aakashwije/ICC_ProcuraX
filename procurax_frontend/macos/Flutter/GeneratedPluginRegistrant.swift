eaaed1a3fe39a9cc124d98a0cb234dd56247dda9
Aakash Wijesekara <aakashwije92@gmail.com>
2026-03-13 20:46:31 +0530

    Register open_file plugin on macOS/Linux
    Add OpenFile plugin registration for macOS and Linux: import open_file_mac on macOS and open_file_linux on Linux, register OpenFilePlugin/OpenFileLinuxPlugin in the generated plugin registrant files, and include open_file in generated_plugins.cmake so the plugin is built into desktop targets.

--- procurax_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ procurax_frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_picker
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import open_file_mac
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
